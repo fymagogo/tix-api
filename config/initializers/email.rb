d7b3be2002a4c733b46cb183ba83c08c162a0c93
Fidel Agogo <fidelagogo@yahoo.com>
2026-02-01 23:54:27 +0000

    fix(email): remove broken email initializer
    SendGrid delivery is already configured in production.rb.
    The delivery_system= method doesn't exist on Devise::Mailer.

--- config/initializers/email.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Configure Devise to use ActiveJob for async emails
-Rails.application.config.to_prepare do
-  Devise::Mailer.delivery_system = :sendgrid if Rails.env.production?
-end
