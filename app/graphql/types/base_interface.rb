1350e01ee62a7ab99830a6e72b630b6560d6ac2b
Fidel Agogo <fidelagogo@yahoo.com>
2026-02-01 19:27:23 +0000

    fix: use module instead of class for BaseInterface (Ruby 3.3 compat)

--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module Types
-  class BaseInterface < GraphQL::Schema::Interface
+  module BaseInterface
+    include GraphQL::Schema::Interface
+
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
